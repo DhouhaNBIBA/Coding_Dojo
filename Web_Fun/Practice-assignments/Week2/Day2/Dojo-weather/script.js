3c0fbf070ac36322c1b76d7c65b318f2ad10e021
DhouhaNBIBA <dhouha.nbiba@gmail.com>
2022-10-23 17:25:56 +0100

    first-commit

---
+++ Web_Fun/Practice-assignments/Week2/Day2/Dojo-weather/script.js
@@ -0,0 +1,21 @@
+// var text= document.querySelector('#txt')
+// function contry(){
+//     alert(text.innerText);
+// }
+// var text2= document.querySelector('#txt2')
+// function contry2(){
+//     alert(text2.innerText);
+// }
+// var text3= document.querySelector('#txt3')
+// function contry3(){
+//     alert(text3.innerText);
+// }
+// var txt= document.querySelector('#txt')
+function alerting(){
+    alert("Loading weather report...");
+}
+remv=document.querySelector('.block')
+function remove(){
+    remv.remove('this');
+
+}
